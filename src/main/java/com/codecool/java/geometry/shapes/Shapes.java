12e37b0558afa7cbc3ed2c4037155197465ad19a
Pawel Popowicz <p.p.popowicz@gmail.com>
2018-07-14 23:38:15 +0200

    add unicode representation of maths symbols

--- src/main/java/com/codecool/java/geometry/shapes/Shapes.java
+++ src/main/java/com/codecool/java/geometry/shapes/Shapes.java
@@ -5,12 +5,14 @@ public enum Shapes{
     ,RECTANGLE("a*b", "2a+2b")
     ,SQUARE("a^2", "4a")
     ,TRIANGLE("sqrt(s(s-a)(s-b)(s-c)), where s = (a+b+c)/2","a+b+c")
-    ,EQUILATERAL_TRIANGLE("sqrt(s(s-a)(s-b)(s-c)), where s = (a+b+c)/2","3*a")
-    ,REGULAR_PENTAGRAM("a^2/4 × sqrt(25+ 10sqrt(5))","5*a");
+    ,EQUILATERAL_TRIANGLE("1/2a×h = (a^2sqrt3)/4","3*a")
+    ,REGULAR_PENTAGON("a^2/4 × sqrt(25+ 10sqrt5)","5*a");
 
     
     private String area;
     private String perimeter;
+    private final String power2 = String.valueOf('\u00B2');
+    private final String sqrt = String.valueOf('\u221A');
     
     private Shapes(String area, String perimeter){
         this.area = area;
@@ -18,7 +20,7 @@ public enum Shapes{
     }
         
     public String getArea(){
-        return area;
+        return area.replace("^2", power2).replace("sqrt", sqrt);
     }
 
     public String getPerimeter(){
